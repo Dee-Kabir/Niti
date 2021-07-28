ac189357b45d582265f9210d99398727a4a703e3
LAPTOP-2NH5B2GR\Deepanshu yadav <kabirsngh0023@gmail.com>
2021-07-28 12:14:24 +0530

    firebase keys

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/database";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAM0SDMpxRo059qPkcgs80LeW9LZ72wEQE",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "teleniti.firebaseapp.com",
   projectId: "teleniti",
   storageBucket: "teleniti.appspot.com",
-  messagingSenderId: "397390101201",
-  appId: "1:397390101201:web:09c09ce5a94a906b9a63f0"
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
